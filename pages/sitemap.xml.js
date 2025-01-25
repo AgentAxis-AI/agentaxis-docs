03fd6b02f285051914cb88c28747fdaa2ba267b3
kodaneflash <jamesonmarcher@gmail.com>
2025-01-25 16:33:25 -0500

    e

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,27 @@
+export async function getServerSideProps({ res, req }) {
+  const baseUrl = `https://${req.headers.host}`
+  const pages = Object.keys(require('./_meta.json'))
+  
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${['', ...pages].map(path => `
+        <url>
+          <loc>${baseUrl}/${path.replace('index', '')}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>daily</changefreq>
+          <priority>0.8</priority>
+        </url>
+      `).join('')}
+    </urlset>
+  `
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  return null
+} 
